5c87dc696e64cbdc35784366f3e9448a733f6b90
KLTIM <131452830+KLTIM@users.noreply.github.com>
2025-12-13 11:56:11 +0300

    Update and rename file 2 to sort.py

---
+++ sort.py
@@ -0,0 +1,8 @@
+# Исходный массив (список)
+original_array = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+
+# Создание нового отсортированного массива с помощью sorted()
+sorted_array = sorted(original_array)
+
+print(f"Оригинальный массив: {original_array}")
+print(f"Новый отсортированный массив: {sorted_array}")
